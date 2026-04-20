dff43be766a08f5ae783dfb181a860bfa0d677fc
RohitYandigeri <rohityandigeri2004@gmail.com>
2026-04-21 01:09:28 +0530

    fixed port binding

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,5 @@ def home():
     return "Cloud Computing Lab Project - Deployed on PaaS!"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
